922877d5993820faa1fb2dafd898499c439c03c9
L3MVN <llyons305@gmail.com>
2024-06-11 19:42:09 -0700

    Commented out the generic view in HW08 just for when I come back

--- Hw08/guitars/views.py
+++ Hw08/guitars/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView
-from Django.views.generic.edit import CreateView, DeleteView, UpdateView
+#from django.views.generic import ListView, DetailView
+#from Django.views.generic.edit import CreateView, DeleteView, UpdateView
 from .models import Department
 
 # Create your views here.
@@ -11,8 +11,8 @@ def index(request):
     context['departments'] = Department.objects.order_by('name','location')[:12]
     return render(request, 'guitars/index.html', context)
 
-class DepartmentListView(ListView):
-    model = Department
+#class DepartmentListView(ListView):
+#    model = Department
 
 def about(request):
     context = {}
